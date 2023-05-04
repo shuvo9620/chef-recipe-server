9a3c41dd9aaa0b571be9dded2ad18d54a10b582f
shuvo9620 <smahfuzur0@gmail.com>
2023-05-04 17:35:07 +0600

    data updated

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+
+const chefs = require('./data/chefs.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Chef is Cooking!')
-  })
+})
 
-  app.listen(port, () => {
+app.get('/chef/:id',(req,res)=>{
+    const id = req.params.id;
+    const chef_info = chefs.find(n => n.id == id);
+    res.send(chef_info);
+    console.log(id);
+
+})
+
+app.get('/chefs', (req, res) =>{
+    res.send(chefs);
+})
+
+app.listen(port, () => {
     console.log(`Chef api is running on port ${port}`)
-  })
+})
